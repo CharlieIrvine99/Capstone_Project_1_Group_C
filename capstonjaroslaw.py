d532b4c8f5c21689fc24618c2bdd4a9ee5d18526
Jaroslaw Glod <57961700+Jarekgd@users.noreply.github.com>
2023-07-10 23:30:38 +0100

    Update capstonjaroslaw.py
    I have changed timer, now user can input the answer before 5 seconds, but also allows to answer after the time

--- capstonjaroslaw.py
+++ capstonjaroslaw.py
@@ -15,50 +15,56 @@ answer = [['London', 'Paris', 'Rome', 'Berlin'], ['2', '4', '8', '16'],
           ]]
 correct_answer = ['a', 'b', 'a', 'c', 'c']
 
-print("Let's play the game...")
-time.sleep(1)
+player = input('What is your name?')
+print("Let's play the game...", player)
+time.sleep(3)
 print(
   'You get 1 point for each correct answer and lose 1 point for a wrong answer.'
 )
-time.sleep(1)
+time.sleep(3)
 print('Choose the answer: a, b, c or d.')
-time.sleep(1)
+time.sleep(3)
+print('You have 10 second to answer.')
+time.sleep(3)
 input("Press enter to continue")
 
 # function to show countdown time
-x = 5                                                          # start countdown from 5 scond
-def timing(x):                                                  
-  while x:                                                      # loop every second                          
-    timer = '{:02d}'.format(x)                                  # formating time to two digit integer
-    print('Your have 5 seconds to answer:', timer, end="\r")    # displaying message and time
-    time.sleep(1)                                               # stopping timer every second
-    x -= 1                                                      # step down every second
+x = 10                                                          # start countdown from 5 scond
+
+def timing(x):
+  while x >= 0:  # loop every second
+    timer = '{:02d}'.format(x)                                # formating time to two digit integer
+    print('', timer, end="\r")                                # displaying message and time
+    time.sleep(1)                                            # stopping timer every second
+    x -= 1                                                    # step down every second
 
 # function declaration to calculate score, show messages and user input
 
 def quiz():
-  score = 0                                                    # start with score = 0
-  k = 0                                                         # first answer
+  score = 0                                                # start with score = 0
+  k = 0                                                    # first answer
   print('\nQuestion 1:\n')
-  for i in question:                                          # loop through all the questions
-    print(i)                                                  # print the question
-    a = 97                                                    # ASCII value of letter 'a'                      
-    for j in answer[k]:                                      # loop through answers corresponding to the question
+  for i in question:                                        # loop through all the questions
+    print(i)                                                # print the question
+    a = 97                                                  # ASCII value of letter 'a'
+    for j in answer[k]:                                    # loop through answers corresponding to the question
       char = chr(a)                                          # character representation of ASCII 97
-      print(char + '.', j)                                    # printing succeding letter for the questions
-      a += 1                                                  # next letter number in alphabet
-    print('\n')                                              # break line
-    timing(x)                                                # function timing() nested into function quiz()
-    your_answer = input('\nYour answer is? : ')              # user answer input 
-    
+      print(char + '.', j)                                  # printing succeding letter for the questions
+      a += 1                                              # next letter number in alphabet
+    print('\n')                                            # break line
+    timing(x)                                              # function timing() nested into function quiz()
+    your_answer = input('\nYour answer is: ')              # user answer input
+
     if your_answer == correct_answer[k]:                    # checking if answer is correct
       score += 1                                            # addin 1 point to the score if correct
       k += 1                                                # going to the next question in the loop
       print(f'Correct, Your score is {score}\n')            # presenting updated score
+      time.sleep(3)
     else:
-      score -= 1                                            # substracting 1 pint if answer was wrong
-      print(f'Wrong, you lose 1 point. Your score is {score}\n')      # updated score
-  print(f'Your final score is {score}')                     # printing final score
-quiz()                                                      # calling the function
+      score -= 1                                              # substracting 1 pint if answer was wrong
+      print(
+        f'Wrong, you lose 1 point. Your score is {score}\n')  # updated score
+      time.sleep(3)
+  print(f'Your final score is {score}')                        # printing final score
 
-                      
+quiz()                                                          # calling the function
