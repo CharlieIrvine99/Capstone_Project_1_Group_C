167bd8fd4fb6735d7b5533239b865adf162eb026
Charlie <121645746+BeefMan99@users.noreply.github.com>
2023-07-13 15:38:48 +0100

    Updated Capstone quiz, after me Delawar and Harbi went through on group call in discord.
    Should be final quiz.

--- Capstone 1 Quiz.py
+++ Capstone 1 Quiz.py
@@ -1,9 +1,9 @@
 import time
 
 pointTotal = 0
-timeInbetweenText = 3
+timeInbetweenText = 3  # change this back to 3
 timeToWait = 3
-x = 3
+x = 3  # change this back to 3
 
 
 def readyForNextQuestion():
@@ -15,6 +15,7 @@ def readyForNextQuestion():
     else:
         print("Input not recognised")
         readyForNextQuestion()
+
 
 def readyForFirstQuestion():
     userReady = input("\nPlease press Enter to start")
@@ -37,6 +38,7 @@ def timing(x):
 
 
 class Question:
+
     def __init__(self, questionNumber, questionAsked, potentialAnswers,
                  correctAnswer, questionBounty, reducedBounty):
         self.questionNumber = questionNumber
@@ -48,7 +50,9 @@ class Question:
 
     def askQuestion(self):
         global pointTotal
-        print(f"\nQuestion {self.questionNumber} is worth {self.questionBounty} points")
+        print(
+            f"\nQuestion {self.questionNumber} is worth {self.questionBounty} points"
+        )
         print(f"Question Number {self.questionNumber}\n{self.questionAsked}")
         for answerLetter, answer in self.potentialAnswers.items():
             print(f"{answerLetter}: {answer}")
@@ -61,7 +65,7 @@ class Question:
             return
         else:
             print(f"That is incorrect. You can however have one more attempt, "
-                  f"you're still playing for {self.reducedBounty} point.")
+                  f"you're still playing for {self.reducedBounty} points.")
             userAnswer2 = input("Enter your answer here: ")
             if userAnswer2.upper() in self.correctAnswer:
                 pointTotal += self.reducedBounty
@@ -72,8 +76,10 @@ class Question:
                 return
             else:
                 correct_answer_formatted = ': '.join(self.correctAnswer)
-                print(f"Sorry, you FAILED! The correct answer was {correct_answer_formatted}\n"
-                      f"Onto the next question.\n\nYour point total is currently {pointTotal} points.\n")
+                print(
+                    f"Sorry, you FAILED! The correct answer was {correct_answer_formatted}\n"
+                    f"Onto the next question.\n\nYour point total is currently {pointTotal} points.\n"
+                )
                 print("Are you ready for your next question?")
                 readyForNextQuestion()
                 return
@@ -81,7 +87,8 @@ class Question:
     def askFinalQuestion(self):
         global pointTotal
         print(
-            f"\nQuestion {self.questionNumber} is worth {self.questionBounty} points")
+            f"\nQuestion {self.questionNumber} is worth {self.questionBounty} points"
+        )
         print(f"Question Number {self.questionNumber}\n{self.questionAsked}")
         for answerLetter, answer in self.potentialAnswers.items():
             print(f"{answerLetter}: {answer}")
@@ -92,7 +99,7 @@ class Question:
             return
         else:
             print(
-                f"That is incorrect. You can however have one more attempt, you're still playing for {self.reducedBounty} point."
+                f"That is incorrect. You can however have one more attempt, you're still playing for {self.reducedBounty} points."
             )
             userAnswer2 = input("Enter your answer here: ")
             if userAnswer2.upper() in self.correctAnswer:
@@ -108,9 +115,12 @@ class Question:
                 )
                 return
 
+
 def userPickName():
     name = input("Enter your name coward: ")
-    nameCorrect = input(f"Okay, so {name.capitalize()} is your name?\nPlease re-enter your name to confirm and continue: ")
+    nameCorrect = input(
+        f"Okay, so {name.capitalize()} is your name?\nPlease re-enter your name to confirm and continue: "
+    )
     if nameCorrect.lower() == name.lower():
         return name
     else:
@@ -120,57 +130,98 @@ def userPickName():
 name = userPickName().capitalize()
 print(f"\nHello & Welcome to the greatest quiz known to mankind, {name}.")
 time.sleep(timeInbetweenText)
-print("\nThe rules are simple, you will get 2 attempts to answer a question correctly.")
+print(
+    "\nThe rules are simple, you will get 2 attempts to answer a question correctly."
+)
 time.sleep(timeInbetweenText)
 print("\nThere is an easy quiz and a hard quiz...")
 time.sleep(timeInbetweenText)
 print(
-    "\nIf you choose the Easy quiz, you will get 5 points for each correct answer, otherwise, you're scraping the barrel for a consolation point.\n")
+    "\nIf you choose the Easy quiz, you will get 5 points for each correct answer, otherwise, you're scraping the barrel for a consolation point.\n"
+)
 time.sleep(timeInbetweenText)
-print("If you choose the Hard quiz, you will get 10 points, and only 5 if you need to try again.\n"
-      "\nAt the end, we will see if you have accumulated enough to be crowned a Champion. Let the games begin!\n")
+print(
+    "If you choose the Hard quiz, you will get 10 points, and only 5 if you need to try again.\n"
+    "\nAt the end, we will see if you have accumulated enough to be crowned a Champion. Let the games begin!\n"
+)
 time.sleep(timeInbetweenText)
 
+easyQuestionOne = Question("1", "How many toes does a human have?", {
+    "A": "8",
+    "B": "9",
+    "C": "10",
+    "D": "11"
+}, ["C", "10"], 5, 2)
 
-easyQuestionOne = Question("1", "How many toes does a human have?",
-                           {"A": "8", "B": "9", "C": "10", "D": "11"},
-                           ["C", "10"], 5, 1)
-
-easyQuestionTwo = Question("2", "What is the capital of France?",
-                           {"A": "London", "B": "Madrid", "C": "Berlin", "D": "Paris"},
-                           ["D", "PARIS"], 5, 1)
+easyQuestionTwo = Question("2", "What is the capital of France?", {
+    "A": "London",
+    "B": "Madrid",
+    "C": "Berlin",
+    "D": "Paris"
+}, ["D", "PARIS"], 5, 2)
 
 easyQuestionThree = Question("3", "Which fruit shares its name with a colour?",
-                             {"A": "Orange", "B": "Starfruit", "C": "Banana", "D": "Apple"},
-                             ["A", "ORANGE"], 5, 1)
+                             {
+                                 "A": "Orange",
+                                 "B": "Starfruit",
+                                 "C": "Banana",
+                                 "D": "Apple"
+                             }, ["A", "ORANGE"], 5, 2)
 
-easyQuestionFour = Question("4", "Who won the most gold medals in history?",
-                            {"A": "Michael Phelps", "B": "Usain Bolt", "C": "Larisa Latynina", "D": "Christ Hoy"},
-                            ["A", "MICHAEL PHELPS"], 5, 1)
+easyQuestionFour = Question(
+    "4", "Who won the most gold medals in history?", {
+        "A": "Michael Phelps",
+        "B": "Usain Bolt",
+        "C": "Larisa Latynina",
+        "D": "Christ Hoy"
+    }, ["A", "MICHAEL PHELPS"], 5, 2)
 
-easyQuestionFive = Question("5", "In the game of chess, which piece can only move diagonally?",
-                            {"A": "Rook", "B": "Knight", "C": "Bishop", "D": "King"},
-                            ["C", "ROOK"], 5, 1)
+easyQuestionFive = Question(
+    "5", "In the game of chess, which piece can only move diagonally?", {
+        "A": "Rook",
+        "B": "Knight",
+        "C": "Bishop",
+        "D": "King"
+    }, ["C", "ROOK"], 5, 2)
 
-hardQuestionOne = Question("1", "What is it called when a function is defined in a class?",
-                           {"A": "Module", "B": "Class", "C": "Function", "D": "Method"},
-                           ["D", "METHOD"], 10, 5)
+hardQuestionOne = Question(
+    "1", "What is it called when a function is defined in a class?", {
+        "A": "Module",
+        "B": "Class",
+        "C": "Function",
+        "D": "Method"
+    }, ["D", "METHOD"], 10, 5)
 
-hardQuestionTwo = Question("2", "Who developed the Python language?",
-                           {"A": "Albert Einstein", "B": "Bill Gates", "C": "Wick Von Rossum", "D": "Gregor Mendel"},
-                           ["C", "WICK VON ROSSUM"], 10, 5)
+hardQuestionTwo = Question(
+    "2", "Who developed the Python language?", {
+        "A": "Albert Einstein",
+        "B": "Bill Gates",
+        "C": "Wick Von Rossum",
+        "D": "Gregor Mendel"
+    }, ["C", "WICK VON ROSSUM"], 10, 5)
 
-hardQuestionThree = Question("3", "Which of the following is the correct extension of the Python file?",
-                             {"A": ".python", "B": ".py", "C": ".p", "D": "None of these"},
-                             ["B", ".PY"], 10, 5)
+hardQuestionThree = Question(
+    "3", "Which of the following is the correct extension of the Python file?", {
+        "A": ".python",
+        "B": ".py",
+        "C": ".p",
+        "D": "None of these"
+    }, ["B", ".PY"], 10, 5)
 
-hardQuestionFour = Question("4", "What do we use to define a block of code in Python?",
-                            {"A": "Indentation", "B": "Key", "C": "Curly Brackets", "D": "Square Brackets"},
-                            ["A", "INDENTATION"], 10, 5)
+hardQuestionFour = Question(
+    "4", "What do we use to define a block of code in Python?", {
+        "A": "Indentation",
+        "B": "Key",
+        "C": "Curly Brackets",
+        "D": "Square Brackets"
+    }, ["A", "INDENTATION"], 10, 5)
 
-hardQuestionFive = Question("5", "Who has the most PyCharm?",
-                            {"A": "Harbi", "B": "Delawar", "C": "Charlie", "D": "Merbaan"},
-                            ["C", "CHARLIE"], 10, 5)
+hardQuestionFive = Question("5", "Who has the most PyCharm?", {
+    "A": "Harbi",
+    "B": "Delawar",
+    "C": "Charlie",
+    "D": "Merbaan"
+}, ["C", "CHARLIE"], 10, 5)
 
 
 def easyQuiz():
@@ -180,9 +231,13 @@ def easyQuiz():
     easyQuestionThree.askQuestion()
     easyQuestionFour.askQuestion()
     easyQuestionFive.askFinalQuestion()
-    print("Congratulations on completing the quiz! Your final score is...\n*Drumroll*")
+    print(
+        "Congratulations on completing the quiz! Your final score is...\n*Drumroll*"
+    )
     timing(5)
-    print(f"{pointTotal}out of 50!\nThank you very much for playing {name}!")
+    print(
+        f"{pointTotal} out of 25 on the easy quiz!\nThank you very much for playing {name}!"
+    )
 
 
 def hardQuiz():
@@ -192,9 +247,13 @@ def hardQuiz():
     hardQuestionThree.askQuestion()
     hardQuestionFour.askQuestion()
     hardQuestionFive.askFinalQuestion()
-    print("Congratulations on completing the quiz! Your final score is...\n*Drumroll*")
+    print(
+        "Congratulations on completing the quiz! Your final score is...\n*Drumroll*"
+    )
     timing(5)
-    print(f"{pointTotal}out of 50!\nThank you very much for playing {name}!")
+    print(
+        f"{pointTotal} out of 50 on the hard quiz!\nThank you very much for playing {name}!"
+    )
 
 
 def chooseDifficulty():
@@ -212,7 +271,26 @@ def chooseDifficulty():
 
     else:
         print("Sorry input unrecognized, please enter: Easy or Hard")
-        chooseDifficulty()  # Here it restarts the loop if the user entered it wrong.
+        chooseDifficulty(
+        )  # Here it restarts the loop if the user entered it wrong.
 
 
 chooseDifficulty()
+score = str(pointTotal)
+
+import csv
+
+
+def saveHighScore(name, score):
+    with open('highScores.csv', 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([name, score])
+
+
+saveHighScore(name, score)
+
+print("Here are the scores of everyone who has played this game!")
+with open('highScores.csv') as file:
+    scores = csv.reader(file)
+    for row in scores:
+        print(row)
